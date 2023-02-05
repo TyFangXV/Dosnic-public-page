3d7889cd03147ac1e4bc95eac59f66de28470be1
TyfangXV <9fangdev@gmail.com>
2023-02-05 22:36:03 +0400

    Bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { PT_Sans } from '@next/font/google'
 import styles from './page.module.css'
+import axios from 'axios';
 import { useEffect, } from 'react';
-import Download_btn from '../components/dl-btn'
+
 
 const PTSans = PT_Sans({
   weight: ["400","700", "400"],
@@ -10,7 +11,22 @@ const PTSans = PT_Sans({
 })
 
 
-export default function Home() {
+async function getData() {
+  const {data, status} = await axios.get('https://s3.ap.cloud-object-storage.appdomain.cloud/dosnic/.dosnic.app_data.json');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (status !== 200) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data...');
+  }
+
+  return JSON.parse(JSON.stringify(data))
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div style={{backgroundColor : "#1E2019"}}> 
       <main className={PTSans.className}>
@@ -18,7 +34,9 @@ export default function Home() {
           <div className={styles.headerContent}>
             <h1>Dosnic</h1>
             <h2>The ultimate movie streaming app</h2>
-             <Download_btn/>
+            <div className={styles.headerButtonContainer}>
+                <a className={styles.downloadButton} download href={`https://dosnic-dl.kryx.cf/dosnic/${data.download_file_name}`}>Download</a>
+            </div>
             <p style={{marginTop: "5vh"}} className={styles.btmNav}>
               <a href="#alert">Alerts</a>
               <a href="#">Devlog</a>
